d62cad7ec1012882b6cad6b21db45dd8e074c763
Kel Mahum <yamatanokingtheninja@gmail.com>
2022-08-15 16:50:49 +0400

    implement request, add basic components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
+  <menu-button></menu-button>
+  <task-button></task-button>
+
+  <task-cards></task-cards>
 </template>
 
 <script>
+import TaskCards from './components/TaskCards.vue';
+import TaskButton from './components/TaskButton.vue';
+import MenuButton from './components/MenuButton.vue';
 
 export default {
   name: 'App',
-  components: {}
+
+  data(){
+    return {
+      cards: []
+    }
+  },
+
+  methods: {
+    async getTasksData(){
+      try {
+        const params = new URLSearchParams({ pagingCount: 10, pagingAfter: 0 })
+
+        let response = await fetch(`http://api.staging.umeu.app/test/tasks/search?${params}`);
+
+        let data = await response.json();
+        
+        this.setTasks(data);
+      }
+      catch(err){
+        console.log(err)
+      }
+    },
+
+    setTasks(data){
+      let { result: { offers } } = data
+      this.cards = offers;
+    }
+  },
+
+  mounted(){
+    this.getTasksData()
+
+  },
+
+  components: { TaskCards, TaskButton, MenuButton }
 }
 </script>
 
-<style lang="scss">
+<style>
 </style>
