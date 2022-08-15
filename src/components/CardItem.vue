3aab766f1c394b06d818758fb7b3762bbf649ba7
Kel Mahum <yamatanokingtheninja@gmail.com>
2022-08-16 03:37:28 +0400

    modify params/units in card-item component

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="card">
-    <h3 class="card__title">{{ validTitle(task.title) }}</h3>
-    <div class="card__date">Дата начала: {{ validDate(task.dateBegin) }}</div>
-    <div class="card__date">Дата окончания: {{ validDate(task.dateEnd) }}</div>
-    <p class="card__category">Категория: {{ task.category }}</p>
-    <div class="card__price">{{ task.price }} руб.</div>
+    <h3 class="card__title">{{ setValidUnits(task.title, 'title') }}</h3>
+    <div class="card__date">Дата начала: {{ setValidUnits(task.dateBegin, 'date') }}</div>
+    <div class="card__date">Дата окончания: {{ setValidUnits(task.dateEnd, 'date') }}</div>
+    <p class="card__category">Категория: {{ setValidUnits(task.category) }}</p>
+    <div class="card__price">{{ setValidUnits(task.price, 'price') }}</div>
   </div>
 </template>
 
@@ -13,16 +13,26 @@ export default {
   props: ['task'],
 
   methods: {
-    validDate(date){
-      if(date){
-        let options = { year: 'numeric', month: 'long', day: 'numeric', hour: "numeric", minute: "numeric"} 
-        return new Date(date).toLocaleDateString("ru-RU", options)
-      } 
-      return 'договорная'
+    setValidUnits(value, type = 'default', unit = 'руб.'){
+      switch(type){
+        case 'title': {
+          return value[0].toUpperCase() + value.slice(1).toLowerCase()
+        }
+        case 'date': {
+          if(value){
+            let options = { year: 'numeric', month: 'long', day: 'numeric', hour: "numeric", minute: "numeric"} 
+            return new Date(value).toLocaleDateString("ru-RU", options)
+          }
+          return 'договорная'
+        }
+        case 'price': {
+          return value.toLocaleString() + '\n' + unit;
+        }
+        default: {
+          return value.toLowerCase()
+        }
+      }
     },
-    validTitle(title){
-      return title[0].toUpperCase() + title.slice(1).toLowerCase()
-    }
   }
 }
 </script>
