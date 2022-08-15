a258abf1c49b60efcd686ea8a8d5dbe3ddc35c46
Kel Mahum <yamatanokingtheninja@gmail.com>
2022-08-16 02:05:26 +0400

    fix card-item component

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="card">
-    <h3 class="card__title">{{ task.title }}</h3>
-    <p class="card__time"></p>
+    <h3 class="card__title">{{ validTitle(task.title) }}</h3>
+    <div class="card__date">Дата начала: {{ validDate(task.dateBegin) }}</div>
+    <div class="card__date">Дата окончания: {{ validDate(task.dateEnd) }}</div>
     <p class="card__category">Категория: {{ task.category }}</p>
     <div class="card__price">{{ task.price }} руб.</div>
   </div>
@@ -9,7 +10,20 @@
 
 <script>
 export default {
-  props: ['task']
+  props: ['task'],
+
+  methods: {
+    validDate(date){
+      if(date){
+        let options = { year: 'numeric', month: 'long', day: 'numeric', hour: "numeric", minute: "numeric"} 
+        return new Date(date).toLocaleDateString("ru-RU", options)
+      } 
+      return 'договорная'
+    },
+    validTitle(title){
+      return title[0].toUpperCase() + title.slice(1).toLowerCase()
+    }
+  }
 }
 </script>
 
